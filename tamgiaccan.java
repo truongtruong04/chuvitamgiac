8077448204fbcc2eade239c43cfb9b29ce44e2c8
truongkhongsai <anhp31746@gmail.com>
2023-04-27 00:15:55 +0700

    fist  commit

---
+++ tamgiaccan.java
@@ -0,0 +1,15 @@
+public class tamgiaccan {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+        int c = 14;
+        if ((a + b > c) || (a + c > b) || (b + c > a)) ;
+        {
+            System.out.println("đây là tam giác !");
+
+            {
+                System.out.println("chu vi tam giac =" + (a + b + c));
+            }
+        }
+    }
+}
